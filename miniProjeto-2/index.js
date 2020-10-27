bb1291fabd129fd908940c76db15207a38949061
NyctibiusVII <matheus.dev.07@gmail.com>
2020-10-27 18:38:04 -0300

    miniProjeto-2

---
+++ miniProjeto-2/index.js
@@ -0,0 +1,10 @@
+// Máximo entre 2 valores
+
+let dataInput_1 = 10
+let dataInput_2 = 10
+
+let whichBigger = max(dataInput_1, dataInput_2)
+
+function max(valor1, valor2) {
+    return valor1 > valor2 ? valor1 : valor2
+}
