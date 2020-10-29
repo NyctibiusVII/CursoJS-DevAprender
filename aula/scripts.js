1e0f36757a23b9d5793c3c2ab1d9b090cae86b99
NyctibiusVII <matheus.dev.07@gmail.com>
2020-10-29 04:37:21 -0300

    arrays

--- aula/scripts.js
+++ aula/scripts.js
@@ -68,3 +68,91 @@ const cloneMouse2 = Object.assign({
 //spread ou 3 pontos
 const cloneMouse3 = {...mouse}
 //console.log(cloneMouse3)
+
+
+//________________________________________________________________
+
+// Arrays
+/**
+add new elementos
+search elementos
+delete elementos
+esvaziar array
+combinar array
+dividir array
+clonar array
+*/
+const numbers = [1,2,3]
+
+// --- ADD --- //
+//inicio
+numbers.unshift(0)//[0,1,2,3]
+//meio
+numbers.slice(1,0,'01')//[0,"01",1,2,3] Ex: insere no index 1, não deleta nada por conta do 0 e add '01'
+//final
+numbers.push(2)//[0,"01",1,2,3,2]
+
+// --- SEARCH --- //
+numbers.indexOf(2)//retorna o índice encontrado do numero 2  >(3)
+numbers.lastIndexOf(2)//retorna o ultimo índice encontrado no numero 2 >(5)
+//retorna -1 se não achar nada!
+numbers.includes(2)//retorna true se achar e false se não achar
+// --- SEARCH | REFERÊNCIA --- //
+const numbersObject = [{
+    id: 1,
+    idade: 18
+},
+{
+    id: 2,
+    idade: 23
+},
+{
+    id: 3,
+    idade: 2
+}]
+const whatIdade = numbersObject.find((numbersIdade) =>{
+    return numbersIdade.idade === 18
+})
+//console.log(whatIdade)//mostra o primeiro bloco de referência (id: 1, idade: 18)
+
+// --- DELETE --- //
+//inicio
+numbers.shift()//deleta o 1° elemento ["01",1,2,3,2]
+//meio
+numbers.splice(2,1)//deleta '1 uma' vez no índice 2 ["01",2,3,2]
+//final
+numbers.pop()//deleta o ultimo elemento ["01",2,3]
+
+// --- ESVAZIAR ARRAY --- //
+let outroNumbers = numbers
+//-1
+//numbers = [] //'re-instânciar' - mas numbers tem que ser 'let'
+//-2 - BEST
+//numbers.length = 0 //APAGANDO TODAS as referências existentes (reduzido a NADA)
+//-3
+//numbers.splice(0,numbers.length) //msm coisa
+//-4
+//while (numbers.length > 0) {
+//    numbers.pop()
+//}
+
+// --- COMBINAR ARRAY --- //
+const primary = [1,2,3]
+const secondary = [4,5,6]
+
+const combinado_01 = primary.concat(secondary)// concatena 1 array no outro
+const combinado_02 = [...primary,'meio',...secondary]// msm coisa - BEST
+
+// --- DIVIDIR ARRAY --- //
+const dividido_01 = combinado.slice(0,3)//pega itens a partir do índice 0 ao (3 (-1)), ou seja até índice 2
+const dividido_02 = combinado.slice(1)//pega todos os itens a partir do índice 1 ao final do array
+const dividido_03 = combinado.slice()//pega todos os itens e passa para a nova variável
+//também copia TODAS as referências de um objeto (CUIDADO)
+
+// --- CLONE ARRAY --- //
+const clonado = [...primary]//spread
+
+// --- JOIN --- //
+const comb = primary.join('.')//adiciona '.' no meio de todos os itens
+const oi = ',o,i,'
+const comb = oi.split(',')//separa frase por ',' e guarda num array
